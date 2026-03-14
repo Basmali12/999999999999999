8069a62e2fe2ccd918fc5116aea47b178a64e1fb
Basmali12 <163114165+Basmali12@users.noreply.github.com>
2026-03-15 00:19:51 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,23 +1,14 @@
 // config.js
-// === إعدادات المتجر (قم بتعديل البيانات هنا لكل زبون) ===
-
 window.MY_STORE_CONFIG = {
   firebase: {
-    apiKey: "AIzaSyBwzzJqPNa08mPBZNMuvExxCJzkToV65rE",
-    authDomain: "project-4623091190980149681.firebaseapp.com",
-    databaseURL: "https://project-4623091190980149681-default-rtdb.firebaseio.com",
-    projectId: "project-4623091190980149681",
-    storageBucket: "project-4623091190980149681.firebasestorage.app",
-    messagingSenderId: "252053182084",
-    appId: "1:252053182084:web:b35a5e3c7d6777e79a94d1",
-    measurementId: "G-H7JJBWJGQR"
+    apiKey: "AIzaSyAEX8BjlYjsHOxDwD-Wu9qyFyIR3Wb6RxQ",
+    authDomain: "mtgr-24718.firebaseapp.com",
+    databaseURL: "https://mtgr-24718-default-rtdb.firebaseio.com",
+    projectId: "mtgr-24718",
+    storageBucket: "mtgr-24718.firebasestorage.app",
+    messagingSenderId: "385919783070",
+    appId: "1:385919783070:web:64f2703ec8cc0fd1c5ab8f"
   },
-
-  cloudinary: {
-    cloudName: "dongmlmoj",
-    uploadPreset: "app_upload"
-  },
-
   security: {
     adminCode: "1001"
   }
